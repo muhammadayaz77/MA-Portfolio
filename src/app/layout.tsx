875d4cc6896203df462164a63105fab771967488
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2025-09-16 11:41:58 +0500

    Update metadata in layout.tsx for improved SEO and branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,27 @@ const inter = Inter({
 
 
 
+
 export const metadata: Metadata = {
-  title: "Muhammad Ayaz",
-  description: "Portfolio of Muhammad Ayaz",
+  title: "Muhammad Ayaz | MERN Stack Developer",
+  description: "Welcome to Ayaz's portfolio. A passionate MERN stack developer skilled in React, Next.js, Node.js, Express.js, PostgreSQL and MongoDB.",
+  keywords: ["Muhammad Ayaz", "MERN Developer", "Full Stack Developer", "React", "Next.js", "Portfolio","PostgreSQL"],
+  openGraph: {
+    title: "Muhammad Ayaz | MERN Stack Developer",
+    description: "Explore Ayaz's portfolio — MERN stack developer skilled in React, Next.js, Node.js, PostgreSQL, and MongoDB.",
+    url: "https://muhammadayaz.vercel.app",
+    siteName: "Muhammad Ayaz Portfolio",
+    images: [
+      {
+        url: "/Muhammad_Ayaz_ProfilePic.jpeg", // add in /public
+        width: 1200,
+        height: 630,
+        alt: "Muhammad Ayaz Portfolio Banner",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  }
 };
 
 export default function RootLayout({
